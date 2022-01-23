7a1ad65806513f780882c6bb44829cb87fa9817d
Sokil <dmytro.sokil@gmail.com>
2022-01-23 16:14:26 +0200

    Update SubscriberId.php

--- src/Entity/SubscriberId.php
+++ src/Entity/SubscriberId.php
@@ -15,6 +15,11 @@ class SubscriberId
      */
     public function __construct(string $value)
     {
+        if (strlen($value) !== 24) {
+            throw new \InvalidArgumentException('Viber suvscriber id must me 24 chars long');
+        }
+
+        
         $this->value = $value;
     }
 
